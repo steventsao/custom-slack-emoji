a92f968d7c40aebeb8697c6b0628832a380f9607
Steven Tsao <steventsao@me.com>
2023-09-22 21:48:31 -0700

    Add aniaml

---
+++ pages/api/cron/generate-animal.ts
@@ -0,0 +1,52 @@
+import getRandomAdjectiveWithNoun from "../../../utils/words-animals";
+import cloudinary from "../../../utils/cloudinary";
+import { createCanvas, loadImage } from "canvas";
+import { NextRequest, NextResponse } from "next/server";
+import { NextApiResponse } from "next";
+import Replicate from "replicate";
+const replicate = new Replicate({
+  auth: process.env.REPLICATE_API_TOKEN,
+});
+
+// Disabled because cloudinary access process.env
+// export const config = {
+//   runtime: "edge",
+// };
+
+// Googles "Golf Terms"
+// Get links from first page
+// Saves to Prisma db
+export default async function handler(req: NextRequest, res: NextApiResponse) {
+  const randomAdjectiveWithNoun = getRandomAdjectiveWithNoun();
+  const prompt = `A TOK emoji of a ${randomAdjectiveWithNoun}`;
+  const output = await replicate.run(
+    "fofr/sdxl-emoji:dee76b5afde21b0f01ed7925f0665b7e879c50ee718c5f78a9d38e04d523cc5e",
+    {
+      input: {
+        prompt,
+      },
+    }
+  );
+  const imageUrl = output[0];
+  const backgroundRemovedImageUrl = (await replicate.run(
+    "cjwbw/rembg:fb8af171cfa1616ddcf1242c093f9c46bcada5ad4cf6f2fbe8b81b330ec5c003",
+    {
+      input: {
+        image: output[0],
+      },
+    }
+  )) as unknown as string;
+  const result = await cloudinary.v2.uploader
+    .upload(backgroundRemovedImageUrl, {
+      folder: process.env.CLOUDINARY_FOLDER,
+    })
+    .then((result) => result)
+    .catch((err) => console.log(err));
+  return res.status(200).send({
+    message: randomAdjectiveWithNoun,
+    output,
+    prompt,
+    imageUrl,
+    noBg: backgroundRemovedImageUrl,
+  });
+}
