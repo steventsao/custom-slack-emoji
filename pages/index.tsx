a291607f1681f3f657ab5a04bb3499806915f810
Steven Tsao <steventsao@me.com>
2023-09-22 21:20:29 -0700

    not clickalbe yet

--- pages/index.tsx
+++ pages/index.tsx
@@ -49,29 +49,29 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
         )}
         <div className="columns-1 gap-4 sm:columns-2 xl:columns-3 2xl:columns-4">
           {images.map(({ id, public_id, format, blurDataUrl }) => (
-            <Link
-              key={id}
-              href={`/?photoId=${id}`}
-              as={`/p/${id}`}
-              ref={id === Number(lastViewedPhoto) ? lastViewedPhotoRef : null}
-              shallow
-              className="after:content group relative mb-5 block w-full cursor-zoom-in after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight"
-            >
-              <Image
-                alt="sticker photo"
-                className="transform rounded-lg transition will-change-auto group-hover:brightness-110"
-                style={{ transform: "translate3d(0, 0, 0)" }}
-                placeholder="blur"
-                blurDataURL={blurDataUrl}
-                src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/t_sticker-outline/${public_id}.${format}`}
-                width={720}
-                height={480}
-                sizes="(max-width: 640px) ,
+            // <Link
+            //   key={id}
+            //   href={`/?photoId=${id}`}
+            //   as={`/p/${id}`}
+            //   ref={id === Number(lastViewedPhoto) ? lastViewedPhotoRef : null}
+            //   shallow
+            //   className="after:content group relative mb-5 block w-full cursor-zoom-in after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight"
+            // >
+            <Image
+              alt="sticker photo"
+              className="transform rounded-lg transition will-change-auto group-hover:brightness-110"
+              style={{ transform: "translate3d(0, 0, 0)" }}
+              placeholder="blur"
+              blurDataURL={blurDataUrl}
+              src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/t_sticker-outline/${public_id}.${format}`}
+              width={720}
+              height={480}
+              sizes="(max-width: 640px) ,
                   (max-width: 1280px) 50vw,
                   (max-width: 1536px) 25vw,
                   25vw"
-              />
-            </Link>
+            />
+            // </Link>
           ))}
         </div>
       </main>
