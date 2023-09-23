779d50d9ee0ca995e3e87c3a0a8b7d12e2a4a89d
Steven Tsao <steventsao@me.com>
2023-09-22 20:34:48 -0700

    sticker outline

--- pages/index.tsx
+++ pages/index.tsx
@@ -63,10 +63,10 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
                 style={{ transform: "translate3d(0, 0, 0)" }}
                 placeholder="blur"
                 blurDataURL={blurDataUrl}
-                src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${public_id}.${format}`}
+                src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/t_sticker-outline/${public_id}.${format}`}
                 width={720}
                 height={480}
-                sizes="(max-width: 640px) 50vw,
+                sizes="(max-width: 640px) ,
                   (max-width: 1280px) 50vw,
                   (max-width: 1536px) 25vw,
                   25vw"
