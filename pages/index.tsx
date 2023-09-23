7dbf28e6bbe11a52dbfa67960fbd17d3a0b09a5a
Steven Tsao <steventsao@me.com>
2023-09-22 19:48:36 -0700

    slow down

--- pages/index.tsx
+++ pages/index.tsx
@@ -66,7 +66,7 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
                 src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${public_id}.${format}`}
                 width={720}
                 height={480}
-                sizes="(max-width: 640px) 100vw,
+                sizes="(max-width: 640px) 50vw,
                   (max-width: 1280px) 50vw,
                   (max-width: 1536px) 25vw,
                   25vw"
