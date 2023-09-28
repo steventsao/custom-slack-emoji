4b43c30b78ee9006616e473addbed77748a222b8
Steven Tsao <steventsao@me.com>
2023-09-27 20:56:04 -0700

    loading theme

---
+++ app/themes/[slug]/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading({ params }) {
+  return (
+    <div className="flex h-screen">
+      <div className="m-auto">
+        <h3>Loading theme...</h3>
+      </div>
+    </div>
+  );
+}
