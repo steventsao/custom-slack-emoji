cf6e15173c02d49bf5a4ad9b86e2fe4e30b6a034
Steven Tsao <steventsao713@gmail.com>
2024-12-10 22:32:37 -0800

    force dynamic

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import { Analytics } from "@vercel/analytics/react";
 import Image from "next/image";
 import { getStickers } from "utils/getStickers";
 
+export const dynamic = "force-dynamic";
+
 export default async function RootLayout({
   children,
 }: {
