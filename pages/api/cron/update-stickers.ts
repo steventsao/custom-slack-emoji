4ff5bf837fdc68e87918d3b88a614dce5864a2e5
Steven Tsao <steventsao713@gmail.com>
2024-12-13 11:21:23 -0800

    match signature

--- pages/api/cron/update-stickers.ts
+++ pages/api/cron/update-stickers.ts
@@ -1,13 +1,14 @@
 import { getStickers } from "utils/getStickers";
 import { kv } from "@vercel/kv";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { NextApiResponse } from "next";
 
 // Cache key for storing stickers data
 const STICKERS_CACHE_KEY = 'recent_stickers';
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
 
 
-export default async function handler(request: Request) {
+export default async function handler(req: NextRequest, res: NextApiResponse) {
     try {
         // Get fresh data
         const stickersData = await getStickers();
